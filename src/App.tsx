0d6a6f1cdeef9e94f87c379357defa2ce31d3d42
Jeff Tran <tranjeff@live.com>
2025-03-25 23:40:17 -0400

    adding button component from tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import ListGroup from "./components/ListGroup";
-
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
+
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
       <ListGroup
         items={items}
         heading="Cities"
         onSelectItem={handleSelectItem}
       ></ListGroup>
+      <Button color="danger" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
     </div>
   );
 }
